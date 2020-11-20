10674b8b328f7a5e45f6a722fe03d150a6500d24
knowledge0603 <23024084@qq.com>
2020-11-20 10:15:50 +0800

    “remark”

---
+++ ZookeeperWindowsService/Constants.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace ZookeeperWindowsService
+{
+    public static class Constants
+    {
+        public static string KafkaVersion
+        {
+            get { return "0.10.2.1"; }
+        }
+
+        public static string KafkaScalaVersion
+        {
+            get { return "kafka_2.12-" + KafkaVersion; }
+        }
+
+        public static string DownloadUrl
+        {
+            get { return "http://apache.claz.org/kafka/" + KafkaVersion + "/" + KafkaScalaVersion + ".tgz"; }
+        }
+
+        public static string TarFileName
+        {
+            get { return KafkaScalaVersion + ".tgz"; }
+        }
+
+        public static string UncompressedTarFileName
+        {
+            get { return KafkaScalaVersion + ".tar"; }
+        }
+
+        public static string RootDirectory
+        {
+            get { return @"C:\";  }
+        }
+
+        public static string RootDirectoryLinux
+        {
+            get { return @"C:/"; }
+        }
+
+        public static string KafkaLocation
+        {
+            get { return RootDirectory + KafkaScalaVersion;  }
+        }
+
+        public static string ZookeeperProcess
+        {
+            get
+            {
+                return AppDomain.CurrentDomain.BaseDirectory + "baseDir\\zookeeper\\bin\\zkServer.cmd ";
+            }
+        }
+        public static string ZookeeperConfig
+        {
+            get { return RootDirectory + @"\" + KafkaScalaVersion + @"\config\zookeeper.properties"; }
+        }
+
+        public static string KafkaConfig
+        {
+            get { return RootDirectory + @"\" + KafkaScalaVersion + @"\config\server.properties"; }
+        }
+
+        public static string DataDir
+        {
+            get { return RootDirectory + KafkaScalaVersion + @"\data"; }
+        }
+
+        public static string LogDir
+        {
+            get { return RootDirectory + KafkaScalaVersion + @"\kafka-logs"; }
+        }
+
+        public static string DataDirLinux
+        {
+            get { return RootDirectoryLinux + KafkaScalaVersion + @"/data"; }
+        }
+
+        public static string LogDirLinux
+        {
+            get { return RootDirectoryLinux + KafkaScalaVersion + @"/kafka-logs"; }
+        }
+    }
+}
---
+++ ZookeeperWindowsService/ZookeeperService.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.ServiceProcess;
+using System.Text;
+
+namespace ZookeeperWindowsService
+{
+    public partial class ZookeeperService : ServiceBase
+    {
+
+        private Process zookeeperProcess = null;
+        public ZookeeperService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            System.IO.Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+            wLog("zookeeper Service Start");
+            startZookeeper();
+        }
+
+        protected override void OnStop()
+        {
+            KillProcessAndChildren(zookeeperProcess.Id);
+            wLog("zookeeper  Service Stop\n");
+        }
+        
+        private void startZookeeper(Object sender = null, EventArgs e = null)
+        {
+             wLog("Zookeeper  service start ");
+             wLog(Constants.ZookeeperProcess);
+            zookeeperProcess = StartProcess(Constants.ZookeeperProcess);
+        }
+        private static Process StartProcess(string command)
+        {
+            var processInfo = new ProcessStartInfo("cmd.exe", "/c " + command)
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                Verb = "runas"
+            };
+
+            return Process.Start(processInfo);
+        }
+        private static void KillProcessAndChildren(int pid)
+        {
+            using (var searcher = new ManagementObjectSearcher("Select * From Win32_Process Where ParentProcessID=" + pid))
+            {
+                var managementObjects = searcher.Get();
+
+                foreach (var obj in managementObjects)
+                {
+                    var managementObject = (ManagementObject)obj;
+                    KillProcessAndChildren(Convert.ToInt32(managementObject["ProcessID"]));
+                }
+
+                try
+                {
+                    var proc = Process.GetProcessById(pid);
+                    proc.Kill();
+                }
+                catch (ArgumentException)
+                {
+                    // Process already exited.
+                }
+            }
+        }
+        private void CloseZookeeper()
+        {
+            wLog("Kill zookeeper  service");
+            if (null == zookeeperProcess)
+                return;
+            zookeeperProcess.Kill();
+            zookeeperProcess.Close();
+            zookeeperProcess = null;
+        }
+        private void wLog(string logStr, bool wTime = true)
+        {
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\ZookeeperAutoService.log", true))
+            {
+                string timeStr = wTime == true ? DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss ") : "";
+                sw.WriteLine(timeStr + logStr);
+            }
+        }
+    }
+}
